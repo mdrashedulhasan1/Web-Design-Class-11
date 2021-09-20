6c57ae49ea4a280b8a8915e3c51479f00e96c457
Rashedul Hasan <Rashedul Hasan>
2021-09-21 05:34:46 +0600

    Simple jQuery

---
+++ function.js
@@ -0,0 +1,12 @@
+function add(num1, num2){
+    var result =num1+num2;
+    console.log(result);
+}
+
+add(3,4);
+add(5,4);
+add(7,4);
+add(9,4);
+add(1,4);
+add(2,4);
+
---
+++ index.js
@@ -0,0 +1,68 @@
+// $(document).ready(function(){
+//     $("#btn1").click(function(){
+//         $("#box").animate({height:"500px"});
+//     });
+//     $("#btn2").click(function(){
+//         $("#box").animate({height:"200px"});
+//     });
+    
+// });
+
+// $(document).ready(function(){
+//     $("#start").click(function(){
+//         $("div").animate({height:600}, 2000);
+//         $("div").animate({width:600}, 2000);
+//         $("div").animate({height:200}, 2000);
+//         $("div").animate({width:200}, 2000);
+
+        
+//     })
+//     $("#stop").click(function(){
+//         $("div").clearQueue();
+//     });
+
+// });
+
+// $(document).ready(function(){
+//     $("#start").click(function(){
+//         $("div").animate({height:300}, 3000);
+//         $("div").animate({width:300}, 3000);
+//     });
+//     $("#complete").click(function(){
+//        $("div").finish();
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#show").click(function(){
+//         $("div").animate({height:400}, 5000);
+//         $("div").animate({width:400}, 5000);
+        
+//     });
+
+//     $("#hide").click(function(){
+//         $("div").hide();
+//     });
+
+//     $("#show").click(function(){
+//         $("div").show();
+//     });
+
+// });
+
+
+// $(document).ready(function(){
+//     $(".btn1").click(function(){
+//         $("h1").slideUp();
+//     });
+//     $(".btn2").click(function(){
+//         $("h1").slideDown();
+//     });
+// });
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("h1").slideToggle();
+    });
+});
+   
